871136810a861a813d2fab3409a36f78b4b1e23f
aster21032003 <119748989+aster21032003@users.noreply.github.com>
2022-12-03 19:30:34 +0200

    Create main.js

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+function a() {
+   var mm = document.getElementById("m").value;
+   var dd = document.getElementById("d").value;
+   var nn = document.getElementById("n").value;
+   
+   
+   document.getElementById('find').value = ("Транспортные услуги по маршруту" +" "+ mm +" "+ "от" +" "+ dd +" "+ "водитель Троценко Владислав Вадимович авто ГАЗ С488УХ190 договор-заявка" + " " + nn  );
+
+   
+}
